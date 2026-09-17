d7dcbebc503987571718f801fad09da73776716c
kittenz <windlolz55@gmail.com>
2026-09-17 14:11:39 +0800

    Fix: Update exercise classifications and names to exactly match video guide (v2.43)

--- exercise_db.js
+++ exercise_db.js
@@ -2,61 +2,81 @@
 
 const EXERCISE_DB = [
     {
-        category: "胸部訓練 (大肌群)",
+        category: "胸部 (大肌群)",
         exercises: [
             { name: "啞鈴臥推", type: "weight", defaultSets: 4, defaultReps: "8下" },
-            { name: "上斜啞鈴臥推", type: "weight", defaultSets: 4, defaultReps: "8下" },
-            { name: "下斜啞鈴臥推", type: "weight", defaultSets: 4, defaultReps: "8下" },
+            { name: "啞鈴上斜臥推", type: "weight", defaultSets: 4, defaultReps: "8下" },
+            { name: "啞鈴下斜臥推", type: "weight", defaultSets: 4, defaultReps: "8下" },
             { name: "啞鈴飛鳥", type: "weight", defaultSets: 3, defaultReps: "10下" }
         ]
     },
     {
-        category: "背部訓練 (大肌群)",
+        category: "背部 (大肌群)",
         exercises: [
             { name: "俯身啞鈴划船", type: "weight", defaultSets: 4, defaultReps: "8下" },
             { name: "單臂啞鈴划船", type: "weight", defaultSets: 4, defaultReps: "8下" },
-            { name: "上斜啞鈴划船", type: "weight", defaultSets: 4, defaultReps: "8下" },
-            { name: "仰臥直臂上拉", type: "weight", defaultSets: 3, defaultReps: "10下" }
+            { name: "上斜啞鈴划船", type: "weight", defaultSets: 4, defaultReps: "8下" }
         ]
     },
     {
-        category: "臀腿訓練 (大肌群)",
+        category: "臀腿 (大肌群)",
         exercises: [
             { name: "高腳杯深蹲", type: "weight", defaultSets: 4, defaultReps: "8下" },
             { name: "啞鈴硬拉", type: "weight", defaultSets: 4, defaultReps: "8下" },
-            { name: "直腿硬拉", type: "weight", defaultSets: 4, defaultReps: "8下" },
+            { name: "啞鈴直腿硬拉", type: "weight", defaultSets: 4, defaultReps: "8下" },
             { name: "保加利亞單腿蹲", type: "weight", defaultSets: 3, defaultReps: "10下" },
-            { name: "啞鈴臀橋", type: "weight", defaultSets: 4, defaultReps: "10下" },
-            { name: "啞鈴單腿提踵 (小腿)", type: "weight", defaultSets: 3, defaultReps: "15下" }
+            { name: "啞鈴臀橋", type: "weight", defaultSets: 4, defaultReps: "10下" }
         ]
     },
     {
-        category: "肩部訓練 (小肌群)",
+        category: "肱三頭肌 (小肌群)",
         exercises: [
-            { name: "啞鈴推舉", type: "weight", defaultSets: 3, defaultReps: "10下" },
-            { name: "阿諾德推舉", type: "weight", defaultSets: 3, defaultReps: "10下" },
-            { name: "啞鈴側平舉", type: "weight", defaultSets: 3, defaultReps: "12下" },
-            { name: "俯身啞鈴側平舉", type: "weight", defaultSets: 3, defaultReps: "12下" },
-            { name: "啞鈴過頂前平舉", type: "weight", defaultSets: 3, defaultReps: "12下" }
-        ]
-    },
-    {
-        category: "手臂訓練 (小肌群)",
-        exercises: [
-            { name: "啞鈴彎舉", type: "weight", defaultSets: 3, defaultReps: "10下" },
-            { name: "上斜彎舉", type: "weight", defaultSets: 3, defaultReps: "10下" },
-            { name: "斜托彎舉", type: "weight", defaultSets: 3, defaultReps: "10下" },
-            { name: "集中彎舉", type: "weight", defaultSets: 3, defaultReps: "10下" },
             { name: "頸後啞鈴臂屈伸", type: "weight", defaultSets: 3, defaultReps: "10下" },
             { name: "俯身臂屈伸", type: "weight", defaultSets: 3, defaultReps: "10下" },
             { name: "鑽石啞鈴臥推", type: "weight", defaultSets: 3, defaultReps: "10下" }
         ]
     },
     {
-        category: "前臂與腰腹 (輔助動作)",
+        category: "肱二頭肌 (小肌群)",
+        exercises: [
+            { name: "啞鈴彎舉", type: "weight", defaultSets: 3, defaultReps: "10下" },
+            { name: "上斜啞鈴彎舉", type: "weight", defaultSets: 3, defaultReps: "10下" },
+            { name: "啞鈴斜托彎舉", type: "weight", defaultSets: 3, defaultReps: "10下" },
+            { name: "集中彎舉", type: "weight", defaultSets: 3, defaultReps: "10下" }
+        ]
+    },
+    {
+        category: "肩部 (小肌群)",
+        exercises: [
+            { name: "啞鈴推舉", type: "weight", defaultSets: 3, defaultReps: "10下" },
+            { name: "阿諾德推舉", type: "weight", defaultSets: 3, defaultReps: "10下" },
+            { name: "啞鈴側平舉", type: "weight", defaultSets: 3, defaultReps: "12下" },
+            { name: "俯身啞鈴側平舉", type: "weight", defaultSets: 3, defaultReps: "12下" }
+        ]
+    },
+    {
+        category: "肩袖 (輔助項)",
+        exercises: [
+            { name: "仰臥直臂上拉", type: "weight", defaultSets: 3, defaultReps: "15下" },
+            { name: "啞鈴過頂前平舉", type: "weight", defaultSets: 3, defaultReps: "15下" }
+        ]
+    },
+    {
+        category: "前臂 (輔助項)",
         exercises: [
             { name: "啞鈴錘式彎舉", type: "weight", defaultSets: 3, defaultReps: "15下" },
-            { name: "背後腕彎舉", type: "weight", defaultSets: 3, defaultReps: "15下" },
+            { name: "背後腕彎舉", type: "weight", defaultSets: 3, defaultReps: "15下" }
+        ]
+    },
+    {
+        category: "小腿 (輔助項)",
+        exercises: [
+            { name: "啞鈴單腿提踵", type: "weight", defaultSets: 3, defaultReps: "15下" }
+        ]
+    },
+    {
+        category: "腰腹 (輔助項)",
+        exercises: [
             { name: "負重捲腹", type: "weight", defaultSets: 3, defaultReps: "15下" },
             { name: "啞鈴傳遞", type: "weight", defaultSets: 3, defaultReps: "15下" }
         ]
